b27a7dff781ca827e8a412a2b7f0b53213c54a01
ApuAdh <apurvadk123@gmail.com>
2023-12-06 20:27:54 -0700

    potato

--- script.js
+++ script.js
@@ -25,3 +25,19 @@ document.getElementById("yes").onclick = function(){
   const hiddenElements = document.querySelectorAll('.hidden');
   hiddenElements.forEach((el) => observer.observe(el));
 }
+
+//dropdown Menu
+let acc = document.getElementsByClassName("accordion");
+let i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    let panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
